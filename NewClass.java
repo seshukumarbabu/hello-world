3e93a398d08a4b64f147cb9e6c670b152f047eb6
Seshu Kumar Bangaru <seshukumar@gmail.com>
2017-10-17 17:54:56 +0530

    Updated

--- NewClass.java
+++ NewClass.java
@@ -1,5 +1,9 @@
 public class NewClass {
 	public static void main(String[] args) {
-		System.out.println("New Class for Testing Branch");
+		System.out.println("New Class for Testing Branch1");
+		System.out.println("New Class for Testing Branch2");
+		System.out.println("New Class for Testing Branch3");
+		System.out.println("New Class for Testing Branch4");
+		System.out.println("New Class for Testing Branch5");
 	}
 }
